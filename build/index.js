38b5826999a929e09808b523ca8b008fff6fa813
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-24 21:04:31 +0300

    init commit with text compilation

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const y = 'hi there you';
+console.log(y);
+function logSmth(x) {
+    console.log(x);
+}
+const num = 1;
+console.log(num);
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const y: string = 'hi there you';
+
+console.log(y);
+
+function logSmth(x: string){
+    console.log(x);
+}
+
+const num:number = 1
+console.log(num);
