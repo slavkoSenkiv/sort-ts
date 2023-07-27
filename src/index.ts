90662052766da1e27c4adc6c143faa9350a282c4
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-27 19:29:30 +0300

    try sork linked list in index ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, -1]);
 const sorter = new Sorter(numbersCollection);
@@ -12,3 +13,12 @@ const sorter1 = new Sorter(charactersCollection);
 sorter1.sort();
 console.log(charactersCollection);
 
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(-4);
+
+const sorter2 = new Sorter(linkedList);
+sorter2.sort();
+linkedList.print();
